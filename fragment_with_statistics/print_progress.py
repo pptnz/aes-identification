4337ca4c7b8018c9b58e2d3b56c8f8172179ecc4
baryberri <baryberri@icloud.com>
2018-01-28 00:35:54 +0900

    Bug fix when printing percentage at print_progress function

--- fragment_with_statistics/print_progress.py
+++ fragment_with_statistics/print_progress.py
@@ -4,7 +4,7 @@ def print_progress(current_level, max_level, bar_length=50):
     num_spaces_to_print = bar_length - num_bars_to_print
     print("|{}>{}| {:3.2f}% ({}/{})".format("=" * num_bars_to_print,
                                             " " * num_spaces_to_print,
-                                            current_percentage,
+                                            current_percentage * 100,
                                             current_level,
                                             max_level),
           end="\r")
