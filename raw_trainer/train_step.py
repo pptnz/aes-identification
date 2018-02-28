89f6d2c40b9d0ebecde8354e9bc2c08bfd3d361a
baryberri <baryberri@icloud.com>
2018-02-28 16:37:32 +0900

    Add epsilon to AdamOptimizer for zero-stability

--- raw_trainer/train_step.py
+++ raw_trainer/train_step.py
@@ -5,5 +5,6 @@ from settings import Settings
 
 settings = Settings("./settings.json")
 learning_rate = settings.read("hyperparameters", "learning_rate")
+epsilon = settings.read("hyperparameters", "epsilon")
 
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
+train_step = tf.train.AdamOptimizer(learning_rate, epsilon=epsilon).minimize(loss, global_step=global_step)
