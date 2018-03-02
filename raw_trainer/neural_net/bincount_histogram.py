c905ceaf97a5aafef2986c9a3b228454a00885f1
baryberri <baryberri@icloud.com>
2018-03-02 12:05:03 +0900

    Implement a new neural net using tf.histogram_fixed_length

---
+++ raw_trainer/neural_net/bincount_histogram.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from input_tensor import input_tensor
+from keep_prob import keep_prob
+from settings import Settings
+
+settings = Settings("./settings.json")
+input_dimension = settings.read("data_info", "input_dimension")
+num_groups = settings.read("data_info", "num_groups")
+data_type = eval(settings.read("hyperparameters", "data_type"))
+
+# input_tensor_casted = tf.cast(input_tensor, tf.int32)
+bfd_casted = tf.map_fn(lambda t: tf.histogram_fixed_width(t, [0.0, 256.0], nbins=64, dtype=data_type),
+                       input_tensor,
+                       dtype=data_type)
+bfd_reshaped = tf.reshape(bfd_casted, [-1, 8, 8, 1])
+
+w1 = tf.get_variable(name='w1', shape=[5, 5, 1, 4], dtype=data_type, initializer=tf.contrib.keras.initializers.he_normal())
+h1 = tf.nn.conv2d(bfd_reshaped, w1, strides=[1, 1, 1, 1], padding='SAME')
+h1_pool = tf.nn.max_pool(h1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+w2 = tf.get_variable(name='w2', shape=[3, 3, 4, 8], dtype=data_type, initializer=tf.contrib.keras.initializers.he_normal())
+h2 = tf.nn.conv2d(h1_pool, w2, strides=[1, 1, 1, 1], padding='SAME')
+h2_pool = tf.nn.max_pool(h2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+h2_flat = tf.reshape(h2_pool, [-1, 32])
+
+w3 = tf.get_variable(name='w3', shape=[32, 8], dtype=data_type, initializer=tf.contrib.keras.initializers.he_normal())
+b3 = tf.Variable(tf.truncated_normal([8], dtype=data_type, stddev=0.1))
+h3 = tf.nn.tanh(tf.matmul(h2_flat, w3) + b3)
+h3_drop = tf.nn.dropout(h3, keep_prob)
+
+w4 = tf.Variable(tf.zeros([8, num_groups], dtype=data_type))
+b4 = tf.Variable(tf.zeros([num_groups], dtype=data_type))
+output_tensor = tf.nn.softmax(tf.matmul(h3_drop, w4) + b4)  # do not change the output tensor name.
