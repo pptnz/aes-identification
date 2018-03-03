a5d31d5ccda36cc850e1e1b59286b22f0b78db36
baryberri <baryberri@icloud.com>
2018-03-03 15:21:31 +0900

    Implement gradient clipping to prevent gradient explosion

--- raw_trainer/train_step.py
+++ raw_trainer/train_step.py
@@ -7,4 +7,7 @@ settings = Settings("./settings.json")
 learning_rate = settings.read("hyperparameters", "learning_rate")
 epsilon = settings.read("hyperparameters", "epsilon")
 
-train_step = tf.train.AdamOptimizer(learning_rate, epsilon=epsilon).minimize(loss, global_step=global_step)
+optimizer = tf.train.AdamOptimizer(learning_rate, epsilon=epsilon)
+gvs = optimizer.compute_gradients(loss)
+capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gvs]
+train_step = optimizer.apply_gradients(capped_gvs, global_step=global_step)
