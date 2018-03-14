75ed047b0f1796c6dcce1115064cff63639a1331
baryberri <baryberri@icloud.com>
2018-03-14 17:38:54 +0900

    Bugfix to record_bytes as files don't have annotation anymore

--- raw_trainer/input_data.py
+++ raw_trainer/input_data.py
@@ -3,6 +3,7 @@ from settings import Settings
 from distribute_data import distribute_plain_data, distribute_encrypted_data
 from data_parser import DataParser
 
+
 settings = Settings("./settings.json")
 
 input_dimension = settings.read("data_info", "input_dimension")
@@ -16,9 +17,8 @@ validation_batch_size = settings.read("test_info", "validation_batch_size")
 test_batch_size = settings.read("test_info", "test_batch_size")
 data_type = eval(settings.read("hyperparameters", "data_type"))
 
-record_bytes = input_dimension + num_groups
+record_bytes = input_dimension + 2
 
-# Train Batch
 plain_train_files, plain_validation_files, plain_test_files = distribute_plain_data()
 encrypted_train_files, encrypted_validation_files, encrypted_test_files = distribute_encrypted_data()
 
@@ -31,14 +31,14 @@ encrypted_validation_queue = tf.train.string_input_producer(encrypted_validation
 plain_test_queue = tf.train.string_input_producer(plain_test_files, shuffle=False)
 encrypted_test_queue = tf.train.string_input_producer(encrypted_test_files, shuffle=False)
 
-plain_train_reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-encrypted_train_reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+plain_train_reader = tf.FixedLengthRecordReader(record_bytes=input_dimension)
+encrypted_train_reader = tf.FixedLengthRecordReader(record_bytes=input_dimension)
 
-plain_validation_reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-encrypted_validation_reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+plain_validation_reader = tf.FixedLengthRecordReader(record_bytes=input_dimension)
+encrypted_validation_reader = tf.FixedLengthRecordReader(record_bytes=input_dimension)
 
-plain_test_reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
-encrypted_test_reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+plain_test_reader = tf.FixedLengthRecordReader(record_bytes=input_dimension)
+encrypted_test_reader = tf.FixedLengthRecordReader(record_bytes=input_dimension)
 
 _, plain_train_value = plain_train_reader.read(plain_train_queue)
 _, encrypted_train_value = encrypted_train_reader.read(encrypted_train_queue)
@@ -64,6 +64,7 @@ encrypted_train_fragment = data_parser.set_data(encrypted_train_value) \
     .cast(data_type) \
     .set_shape([record_bytes]) \
     .get_data()
+
 train_fragments = [plain_train_fragment, encrypted_train_fragment]
 train_selection_mask = tf.reshape(tf.multinomial(tf.log([[10., 10.]]), batch_size), [batch_size])
 train_batch = tf.gather(train_fragments, train_selection_mask)
