aa937c2dbff5a47c4c3ca470cb38c81a173d88e6
baryberri <baryberri@icloud.com>
2018-03-14 17:46:24 +0900

    Add accumulation feature to DataFlags class

--- raw_trainer/data_flags.py
+++ raw_trainer/data_flags.py
@@ -8,3 +8,10 @@ class DataFlags:
     @staticmethod
     def get(key):
         return DataFlags.data[key]
+
+    @staticmethod
+    def add(key, value):
+        if key not in DataFlags:
+            DataFlags[key] = 0
+        DataFlags.data[key] += value
+
