cb7e9d5223e5395f36e72d35b2b759652b583bfd
baryberri <baryberri@icloud.com>
2018-03-14 17:47:03 +0900

    Accumulate num_validation_files and num_test_files at both field

--- raw_trainer/distribute_data.py
+++ raw_trainer/distribute_data.py
@@ -23,6 +23,9 @@ def distribute_plain_data():
     validation_index = int(train_index + (num_files * proportion_validation))
     test_index = int(validation_index + (num_files * proportion_test))
 
+    DataFlags.add("num_validation_files", validation_index - train_index)
+    DataFlags.add("num_test_files", test_index - validation_index)
+
     return file_paths[0:train_index], file_paths[train_index:validation_index], file_paths[validation_index:test_index]
 
 
@@ -45,7 +48,7 @@ def distribute_encrypted_data():
     validation_index = int(train_index + (num_files * proportion_validation))
     test_index = int(validation_index + (num_files * proportion_test))
 
-    DataFlags.put("num_validation_files", validation_index - train_index)
-    DataFlags.put("num_test_files", test_index - validation_index)
+    DataFlags.add("num_validation_files", validation_index - train_index)
+    DataFlags.add("num_test_files", test_index - validation_index)
 
     return file_paths[0:train_index], file_paths[train_index:validation_index], file_paths[validation_index:test_index]
