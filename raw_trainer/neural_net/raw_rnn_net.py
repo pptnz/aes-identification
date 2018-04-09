b39c8a2e7014f778b0cefe9366d4ac90daa1353b
pptnz <eye2988@gmail.com>
2018-04-09 11:33:42 +0900

    Abandon one-hot encoding due to the speed

--- raw_trainer/neural_net/raw_rnn_net.py
+++ raw_trainer/neural_net/raw_rnn_net.py
@@ -9,15 +9,20 @@ settings = Settings("./settings.json")
 input_dimension = settings.read("data_info", "input_dimension")
 num_groups = settings.read("data_info", "num_groups")
 data_type = eval(settings.read("hyperparameters", "data_type"))
-batch_size = settings.read("hyperparameters", "batch_size")
 
-lstm_width = 4096
-lstm_depth = 2
+lstm_width = 512
+lstm_input = 64
+lstm_step = 64
+lstm_depth = 3
 
 num_byte_value = 256
 
-input_tensor_casted = tf.cast(input_tensor, tf.int32)
-input_tensor_one_hot = tf.one_hot(input_tensor_casted, num_byte_value)
+
+# Abandon one-hot encoding due to the speed.
+# input_tensor_casted = tf.cast(input_tensor, tf.int32)
+# input_tensor_one_hot = tf.one_hot(input_tensor_casted, num_byte_value)
+input_tensor_reshaped = tf.reshape(input_tensor, [-1, lstm_input, lstm_step])
+
 
 W = tf.Variable(tf.random_normal([lstm_width, num_groups]))
 b = tf.Variable(tf.random_normal([num_groups]))
@@ -29,7 +34,6 @@ def lstm_cell():
 
 cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell() for _ in range(lstm_depth)])
 
-initial_state = cell.zero_state(batch_size, data_type)
-outputs, states = tf.nn.dynamic_rnn(cell, input_tensor_one_hot, dtype=data_type, initial_state=initial_state)
+outputs, states = tf.nn.dynamic_rnn(cell, input_tensor_reshaped, dtype=data_type)
 
-output_tensor = tf.contrib.layers.fully_connected(outputs[:, -1], 1, activation_fn=tf.sigmoid)
+output_tensor = tf.contrib.layers.fully_connected(outputs[:, -1], num_groups, activation_fn=tf.sigmoid)
