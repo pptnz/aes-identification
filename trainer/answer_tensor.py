bde4d67f76176dcc1f84d515c2cdb3819bc67bcf
baryberri <baryberri@icloud.com>
2018-04-10 22:44:20 +0900

    Remove redundant csv-based trainer

--- trainer/answer_tensor.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from settings import Settings
-
-settings = Settings("./settings.json")
-
-batch_size = settings.read("hyperparameters", "batch_size")
-num_groups = settings.read("data_info", "num_groups")
-
-answer_tensor = tf.placeholder(tf.float32, shape=[batch_size, num_groups])
--- trainer/train_step.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from loss import loss
-from global_step import global_step
-from settings import Settings
-
-settings = Settings("./settings.json")
-learning_rate = settings.read("hyperparameters", "learning_rate")
-
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
