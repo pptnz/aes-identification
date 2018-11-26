8589faf2756274b8cab8284cb6ae8c88325e09ef
Jonghoon Won <baryberri@icloud.com>
2018-11-26 13:07:18 +0900

    Update print format

--- classifier/caffe_python/result_counter.py
+++ classifier/caffe_python/result_counter.py
@@ -7,16 +7,16 @@ class ResultCounter:
         self.result_count[true_type][identified_type] += 1
 
     def print_count(self):
-        print("\t\tPlain\t\tEncrypted")
-        print("Plain\t\t{}\t\t{}".format(self.result_count[0][0], self.result_count[0][1]))
-        print("Encrypted\t\t{}\t\t{}".format(self.result_count[1][0], self.result_count[1][1]))
+        print("\t\tPlain\tEncrypted")
+        print("Plain\t\t{}\t{}".format(self.result_count[0][0], self.result_count[0][1]))
+        print("Encrypted\t{}\t{}".format(self.result_count[1][0], self.result_count[1][1]))
 
     def print_prob(self):
         plain_fragments_count = self.result_count[0][0] + self.result_count[0][1]
         encrypted_fragments_count = self.result_count[1][0] + self.result_count[1][1]
 
-        print("\t\tPlain\t\tEncrypted")
-        print("Plain\t\t{:3.2f}%\t\t{:3.2f}%".format(self.result_count[0][0] / plain_fragments_count * 100,
+        print("\t\tPlain\tEncrypted")
+        print("Plain\t\t{:3.2f}%\t{:3.2f}%".format(self.result_count[0][0] / plain_fragments_count * 100,
                                          self.result_count[0][1] / plain_fragments_count * 100))
-        print("Encrypted\t\t{:3.2f}%\t\t{:3.2f}%".format(self.result_count[1][0] / encrypted_fragments_count * 100,
+        print("Encrypted\t{:3.2f}%\t{:3.2f}%".format(self.result_count[1][0] / encrypted_fragments_count * 100,
                                                          self.result_count[1][1] / encrypted_fragments_count * 100))
